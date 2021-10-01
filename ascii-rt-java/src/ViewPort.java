9c6acdea6eaaf3043f91bd3fae2280138d1409e3
martinlaursen97 <martin_laursen21@hotmail.com>
2021-10-01 02:11:35 +0200

    added some util, ray, camera, screen, sphere

--- ascii-rt-java/src/ViewPort.java
+++ ascii-rt-java/src/ViewPort.java
@@ -12,6 +12,9 @@ public class ViewPort extends JPanel implements ActionListener {
     int size = 7;
     int pixelSize = size + 8;
 
+    Camera camera = new Camera(new Vector3D(0,0,0), new Vector3D(0,0,1));
+    Screen screen = new Screen(camera, 1F);
+
     Timer t = new Timer(1, this);
 
     void run() {
@@ -35,8 +38,26 @@ public class ViewPort extends JPanel implements ActionListener {
         g.setFont(font);
 
 
+        for (int y = 0; y < HEIGHT; y += size) {
+            for (int x = 0; x < WIDTH; x += size) {
+                // Scale coordinates down to screens size
+                float u = (float) x / WIDTH;
+                float v = (float) y / HEIGHT;
 
+                // Point on screen = p0 + (p1 - p0) * u + (p2-p0) * v;
+                Vector3D p1p0 = Util.vectorMultiply(Util.vectorSubtract(screen.p1, screen.p0), u);
+                Vector3D p2p0 = Util.vectorMultiply(Util.vectorSubtract(screen.p2, screen.p0), v);
+                Vector3D point = Util.vectorAdd(Util.vectorAdd(screen.p0, p1p0), p2p0);
 
+                // Shoot ray through point
+                Vector3D vectorDirection = Util.vectorSubtract(point, camera.position);
+
+                Ray ray = new Ray(camera.position, vectorDirection, 10E5F);
+
+                // Trace ray, return pixel data, fill pixel
+
+            }
+        }
         t.start();
     }
 
